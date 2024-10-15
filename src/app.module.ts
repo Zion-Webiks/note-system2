c894eb316d33a0c9b35a6b44faa32270eab14956
Zion-Webiks <zion@webiks.com>
2024-10-15 10:07:54 +0300

    App init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { NoteModule } from './note/note.module';
+
+@Module({
+  imports: [
+    UserModule, 
+    AuthModule, 
+    NoteModule],
+})
+export class AppModule {}
